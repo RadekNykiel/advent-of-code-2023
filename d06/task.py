f7c01fae7096478229da7ddaec9bf943952802a2
Radek Nykiel <rad.nyk93@gmail.com>
2023-12-06 12:22:34 +0100

    Day 6 - refactor, inlining some functions

--- d06/task.py
+++ d06/task.py
@@ -1,34 +1,21 @@
+from _operator import mul
 from functools import reduce, partial
 from operator import mul
 
 
-def calc_distance(race_time, press):
-    speed = press
-    time_to_run = (race_time - press) * speed
-    return time_to_run
-
-
 def solve_file(input_file):
     races = parse_input(input_file)
-    combinations = calc_combinations(races)
+    combinations = reduce(mul, (calc_single_combinations(race1) for race1 in races))
     print(f"For {input_file=} as multiple races got {combinations=}")
     race = parse_input_as_one_race(input_file)
     combinations = calc_single_combinations(race)
     print(f"For {input_file=} as single race got {combinations=}")
 
 
-def calc_combinations(races):
-    ways_to_win = []
-    for race in races:
-        ways_to_win_current = calc_single_combinations(race)
-        ways_to_win.append(ways_to_win_current)
-    combinations = reduce(mul, ways_to_win)
-    return combinations
-
-
 def race_won(race, press_time):
     race_time, race_distance = race
-    return calc_distance(race_time, press_time) > race_distance
+    my_distance = (race_time - press_time) * press_time
+    return my_distance > race_distance
 
 
 def calc_single_combinations(race):
